4a6da71d70658344f83ecfc295b3d6160e7e5082
Dimas Sudjito <almuhtad@ualberta.ca>
2022-05-13 22:50:16 -0600

    add GET list, GET one, DELETE, PUT operation to Movie

--- routes/movies.js
+++ routes/movies.js
@@ -3,8 +3,22 @@ const Movie = require('../models/Movie')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('we are on movies')
+router.get('/', async (req, res) => {
+  try {
+    const movies = await Movie.find()
+    res.json(movies)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+router.get('/:movieId', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.movieId)
+    res.json(movie)
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 router.post('/', async (req, res) => {
@@ -14,6 +28,27 @@ router.post('/', async (req, res) => {
   try {
     const savedMovie = await movie.save()
     res.json(savedMovie)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+router.delete('/:movieId', async (req, res) => {
+  try {
+    const removedMovie = await Movie.remove({ _id: req.params.movieId })
+    res.json(removedMovie)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+router.put('/:movieId', async (req, res) => {
+  try {
+    const updatedMovie = await Movie.updateOne(
+      { _id: req.params.movieId },
+      { $set: req.body }
+    )
+    res.json(updatedMovie)
   } catch (err) {
     res.json({ message: err })
   }
