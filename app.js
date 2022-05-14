024e0d5d5a1fb7b611aac25f6859312ad0cf5c06
Dimas Sudjito <almuhtad@ualberta.ca>
2022-05-13 20:30:23 -0600

    setup mongoose and routes folder

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const moviesRouter = require('./routes/movies')
 
 const PORT = 5000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// connect to mongodb
+var mongoDB =
+  'mongodb+srv://dimas:bonantagon@cluster0.ijepu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// specify routes
+app.use('/movies', moviesRouter)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
